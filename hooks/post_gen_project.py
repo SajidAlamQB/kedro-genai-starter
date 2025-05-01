d495dd4998f538113b3253bef4e796ac1cd31936
L. R. Couto <laurarccouto@gmail.com>
2025-05-01 15:36:45 +0100

    use spaceflights script as reference
    Signed-off-by: L. R. Couto <laurarccouto@gmail.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,26 +1,21 @@
 from pathlib import Path
 
-from kedro.templates.project.hooks.utils import setup_template_tools, sort_requirements
+from kedro.templates.project.hooks.utils import (
+
+    setup_template_tools,
+    sort_requirements,
+)
 
 
-def main(selected_tools):
+def main(example_pipeline):
     current_dir = Path.cwd()
     requirements_file_path = current_dir / "requirements.txt"
     pyproject_file_path = current_dir / "pyproject.toml"
-    python_package_name = "{{ cookiecutter.python_package }}"
-
-    # Get the selected tools from cookiecutter
+    python_package_name = '{{ cookiecutter.python_package }}'
     selected_tools = "{{ cookiecutter.tools }}"
-    example_pipeline = "{{ cookiecutter.example_pipeline }}"
 
     # Handle template directories and requirements according to selected tools
-    setup_template_tools(
-        selected_tools,
-        requirements_file_path,
-        pyproject_file_path,
-        python_package_name,
-        example_pipeline,
-    )
+    setup_template_tools(selected_tools, requirements_file_path, pyproject_file_path, python_package_name, example_pipeline)
 
     # Sort requirements.txt file in alphabetical order
     sort_requirements(requirements_file_path)
@@ -28,5 +23,9 @@ def main(selected_tools):
 
 if __name__ == "__main__":
     # Get the selected tools from cookiecutter
-    selected_tools = "{{ cookiecutter.tools }}"
-    main(selected_tools)
+    example_pipeline = "{{ cookiecutter.example_pipeline }}"
+
+    # Ensure the script doesn't run with kedro new --starter but only with examples selected.
+    # User cannot specify both starter and example.
+    if example_pipeline == "True":
+        main(example_pipeline)
